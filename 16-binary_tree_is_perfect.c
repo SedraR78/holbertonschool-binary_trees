308c319e688628a7223425ce7fa907d51ecc374f
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-07 10:47:56 +0200

    Task 17 : binary tree sibling and 18:binary tree uncle

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+int binary_tree_is_perfect(const binary_tree_t *tree){
+    if (tree == NULL )
+    {
+        return(0);
+    }
+    if (tree->left == NULL &&  tree->right == NULL)
+    {
+        return(1);
+    }
+        
+}   
