0b1fd79f6a18844d03d84894540c7760f1c19a35
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-08 17:21:44 +0200

    fix betty for task 17 : binary tree sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,13 +7,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent){
-        return (NULL);
-    }
-	if (node->parent->left == node){
-        return (node->parent->right);
-    }
-	else{
-        return (node->parent->left);
-    }
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		return (node->parent->left);
+	}
 }
