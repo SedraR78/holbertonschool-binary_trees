bc6999ae85bc332c85f941b066b7329520ce7ae3
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-08 17:17:14 +0200

    Fixing The Betty Of 18: binary tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
-binary_tree_t *sibling(binary_tree_t *node);
+/**
+ * binary_tree_sibling - finds the sibling of a node in a binary tree
+ * @node: node to find sibling for
+ *
+ * Return: pointer to sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
 
 /**
  * binary_tree_uncle - finds the uncle of a node in a binary tree
@@ -10,29 +25,8 @@ binary_tree_t *sibling(binary_tree_t *node);
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node)
-        {
-            return (NULL);
-        }	
-	return (sibling(node->parent));
-}
-
-/**
- * sibling - gets the sibling node to find uncle
- * @node: to get sibling from
- * Return: sibling of node
- */
-binary_tree_t *sibling(binary_tree_t *node)
-{
-	if (!node || !node->parent)
-    {
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-    }
-	if (node->parent->left == node){
-        return (node->parent->right);
-    }
-	else
-		{
-            return (node->parent->left);
-        }
+
+	return (binary_tree_sibling(node->parent));
 }
