95f65fadbd686417abefee7d2ab6317dcc3f24ae
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-08 21:08:10 +0200

    etty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,3 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ * Return: The height of the tree, or 0 if tree is NULL.
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
